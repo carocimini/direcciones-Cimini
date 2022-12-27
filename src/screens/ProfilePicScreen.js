9ddbcd881a10c4f0eb213bf370443012b26c23ba
Carola Cimini <caro.cimini@gmail.com>
2022-12-26 22:12:46 -0300

    fix navegación perfil

--- src/screens/ProfilePicScreen.js
+++ src/screens/ProfilePicScreen.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 const ProfilePicScreen = ({ navigation }) => {
     const handleSave = () => {
-        navigation.navigate("Profile")
+        navigation.navigate("Perfil")
     }
     
     return (
